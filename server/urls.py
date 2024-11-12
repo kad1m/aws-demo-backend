ecbcbb178f8a12c8c4828360c24173042482c286
kad1m <dmytro.lytvynenko@icloud.com>
2024-11-12 12:14:25 +0200

    files

--- server/urls.py
+++ server/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # Include the api urls  
-    path('api2/', include('api.urls')),  # Include the api urls
+    path('api/', include('api.urls')),  # Include the api urls
 ]
