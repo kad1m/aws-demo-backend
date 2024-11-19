b7fb8c2e191cb5781d7230bb3328c69ec72d5253
kad1m <dmytro.lytvynenko@icloud.com>
2024-11-19 10:51:01 +0200

    files

--- api/views.py
+++ api/views.py
@@ -1,9 +1,16 @@
+import os
+
 from django.shortcuts import render, HttpResponse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def index(request):
-    return render(request, 'index.html')
+    text = os.getenv('MESSAGE_DJANGO')
+    return render(request, 'index.html', {'text': text})
 
 
 def apiresponse(request):
+    text = os.getenv('MESSAGE_REACT')
     return HttpResponse('Hello, World for React!')
